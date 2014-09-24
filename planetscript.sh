ed6b0a6724780cea3a3dcb3fb488523629285431
nscatlin <nscatlin@users.noreply.github.com>
2014-09-24 16:24:30 -0400

    Update planetscript.sh

--- planetscript.sh
+++ planetscript.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-for i in Mercury_(planet) Venus Earth Mars Jupiter Saturn Uranus Neptune Pluto
-do 
-curl "http://en.wikipedia.org/wiki/$i"
+for i in Mercury Venus Earth Mars Jupiter Saturn Uranus Neptune Pluto
+do
+curl "http://en.wikipedia.org/wiki/$i" > $i.txt
+if [ -s $i.txt ]
+then
+echo "$i.txt is not empty"
+else
+echo "$i.txt is empty"
+fi
+git add $i.txt
 done
-
+git commit -m "Planet Wiki"
+git push https://nscatlin@github.com/UFCompBio/nscatlin.git
